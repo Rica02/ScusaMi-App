3f51ca654b8f4869f61b0201c3f996f7beafe4d7
Rica Mae Averion <rica@playtoday.cc>
2022-11-01 13:28:45 +1100

    file name change

--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { VALUES } from '../../constants/Styling';
-import MenuItem from './Item';
+import MenuItem from './MenuItem';
 import { MenuItemType } from '../../typings/menuTypes';
 
 interface MenuListProps {
