6dbb2ff70cf2689ebe63841a3ad7ac99a14c2976
Rica Mae Averion <rica@playtoday.cc>
2022-11-07 22:11:52 +1100

    added components to OrderScreen

---
+++ src/components/order/ActiveOrderCard.tsx
@@ -0,0 +1,53 @@
+import { Feather } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { COLOURS } from '../../constants/Colours';
+import { VALUES } from '../../constants/Styling';
+
+interface ActiveOrderCardProps {}
+
+const ActiveOrderCard = (props: ActiveOrderCardProps) => {
+  const {} = props;
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Order placed 10 minutes ago</Text>
+        <Text>Monday, 10th October 2022 at 2:19 pm</Text>
+        <Text>Dine-in at Table 1</Text>
+      </View>
+      <View style={styles.lowerContainer}>
+        <Text style={styles.numItemsText}>3 item(s)</Text>
+        <View style={styles.viewProgressContainer}>
+          <Text style={styles.viewProgressText}>View progress</Text>
+          <Feather name="chevron-right" size={24} color="black" />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ActiveOrderCard;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: VALUES.SPACING.MEDIUM,
+    backgroundColor: COLOURS.WHITE,
+  },
+
+  lowerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: VALUES.SPACING.SMALL,
+  },
+  numItemsText: {
+    color: COLOURS.TEXT_GREY,
+  },
+  viewProgressContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  viewProgressText: {
+    fontWeight: '600',
+  },
+});
--- src/screens/OrderScreen.tsx
+++ src/screens/OrderScreen.tsx
@@ -1,9 +1,61 @@
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  ScrollView,
+} from 'react-native';
+import { useTranslation } from 'react-i18next';
+
+import { COLOURS } from '../constants/Colours';
+import { VALUES } from '../constants/Styling';
+import HeaderTitle from '../components/common/HeaderTitle';
+import OrderButtons from '../components/order/OrderButtons';
+import ActiveOrderCard from '../components/order/ActiveOrderCard';
 
 export default function OrderScreen() {
+  const { t } = useTranslation();
+  const [hasActiveOrders, setHasActiveOrders] = useState<boolean | undefined>();
+
+  useEffect(() => {
+    setHasActiveOrders(true);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Order</Text>
+      <ImageBackground
+        style={styles.upperContainer}
+        source={require('../../assets/images//ui-images/menu-1.png')}
+        imageStyle={{ opacity: 0.5, marginVertical: VALUES.SPACING.LARGE }}
+        resizeMode="cover"
+      >
+        <Text style={styles.startOrderingText}>
+          {t('order.start_ordering')}
+        </Text>
+        <OrderButtons />
+      </ImageBackground>
+      <View style={styles.lowerContainer}>
+        <HeaderTitle colour={COLOURS.WHITE}>
+          {t('order.your_active_orders')}
+        </HeaderTitle>
+        {hasActiveOrders ? (
+          <ScrollView style={styles.activeOrderContainer}>
+            <View style={styles.activeOrderCardContainer}>
+              <ActiveOrderCard />
+            </View>
+            <View style={styles.activeOrderCardContainer}>
+              <ActiveOrderCard />
+            </View>
+          </ScrollView>
+        ) : (
+          <View style={styles.noActiveOrdersContainer}>
+            <Text style={styles.noActiveOrdersText}>
+              {t('order.no_active_orders')}
+            </Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 }
@@ -11,11 +63,43 @@ export default function OrderScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: COLOURS.WHITE,
+  },
+  upperContainer: {
+    flex: 3,
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  startOrderingText: {
+    fontFamily: 'caveat-brush',
+    textAlign: 'center',
+    paddingBottom: VALUES.SPACING.XLARGE,
+    color: COLOURS.RED,
+    fontSize: VALUES.FONT_SIZE['2XLARGE'],
+    textShadowColor: COLOURS.WHITE,
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 0,
+    textTransform: 'uppercase',
+  },
+
+  lowerContainer: {
+    paddingVertical: VALUES.SPACING.MEDIUM,
+    backgroundColor: COLOURS.RED,
+    flex: 1,
+  },
+  noActiveOrdersContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  noActiveOrdersText: {
+    color: COLOURS.BEIGE,
+    fontStyle: 'italic',
+  },
+  activeOrderContainer: {
+    paddingVertical: VALUES.SPACING.SMALL,
+  },
+  activeOrderCardContainer: {
+    paddingHorizontal: VALUES.SPACING.SMALL,
+    paddingVertical: 1.5 * VALUES.SPACING.XSMALL,
   },
 });
